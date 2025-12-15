c1b12c70bd3d33f527e5477a133a85f3d9fd4468
Ritik5629 <ritikrana2804@gmail.com>
2025-12-15 16:01:08 +0530

    Items added in the drawer

--- lib/contact_page.dart
+++ lib/contact_page.dart
@@ -20,48 +20,95 @@ class ContactPage extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        drawer: Drawer(),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.green),
+                child: Text(
+                  "Contact App",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
 
-    appBar: AppBar(
-      backgroundColor: Colors.deepPurple,
+              ListTile(
+                leading: Icon(Icons.contacts),
+                title: Text("Contacts"),
+                // onTap: () {
+                // Navigator.pop(context);
+                // },
+              ),
 
-      title: Container(
-        height: 40,
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        decoration: BoxDecoration(
-          color: Colors.white, 
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: TextField(
-          cursorColor: Colors.deepPurple,
-          decoration: const InputDecoration(
-            hintText: "Search",
-            hintStyle: TextStyle(color: Colors.grey),
-            border: InputBorder.none,
-            icon: Icon(Icons.search, color: Colors.grey),
+              ListTile(leading: Icon(Icons.settings), title: Text("Settings")),
+
+              ListTile(
+                leading: Icon(Icons.lock_clock),
+                title: Text("Call History"),
+              ),
+
+              ListTile(
+                leading: Icon(Icons.help),
+                title: Text("Help & feedback "),
+              ),
+
+              SizedBox(height: 30),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "LOGOUT",
+                    style: TextStyle(fontSize: 14, color: Colors.green),
+                  ),
+                ),
+              ),
+            ],
           ),
-          style: const TextStyle(color: Colors.black),
         ),
-      ),
 
-      actions: const [
-        Icon(Icons.mic, color: Colors.white),
-      ],
-    ),
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
 
+          title: Container(
+            height: 40,
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: TextField(
+              cursorColor: Colors.deepPurple,
+              decoration: const InputDecoration(
+                hintText: "Search",
+                hintStyle: TextStyle(color: Colors.grey),
+                border: InputBorder.none,
+                icon: Icon(Icons.search, color: Colors.grey),
+              ),
+              style: const TextStyle(color: Colors.black),
+            ),
+          ),
+
+          actions: const [Icon(Icons.mic, color: Colors.white)],
+        ),
 
         body: ListView.builder(
           itemCount: contacts.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.deepPurple,
-                child: Text(contacts[index]["name"]![0], style: TextStyle(color: Colors.white)),
-              ),
+            return Card(
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.deepPurple,
+                  child: Text(
+                    contacts[index]["name"]![0],
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
 
-              title: Text(contacts[index]["name"]!),
-              subtitle: Text(contacts[index]["phone"]!),
-              trailing: const Icon(Icons.phone),
+                title: Text(contacts[index]["name"]!),
+                subtitle: Text(contacts[index]["phone"]!),
+                trailing: const Icon(Icons.phone),
+              ),
             );
           },
         ),
